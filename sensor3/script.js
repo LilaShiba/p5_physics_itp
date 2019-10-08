40589b890084a206f561ed43630941d3df6ddf45
kyle1james <kyle1james@gmail.com>
2019-10-08 16:09:41 -0400

    meow

--- sensor3/script.js
+++ sensor3/script.js
@@ -90,7 +90,23 @@ function deviceMotionHandler(yourMotion){
     }
     
   } else{
-    console.log('not working')
+    let sensor = new Gyroscope();
+    sensor.start();
+
+    sensor.onreading = () => {
+        console.log("Angular velocity around the X-axis " + sensor.x);
+        console.log("Angular velocity around the Y-axis " + sensor.y);
+        console.log("Angular velocity around the Z-axis " + sensor.z);
+        var x_accel = sensor.x
+        const accelX = document.querySelector("#x")
+          accelX.innerHTML = "Your X Axis is: " + x_accel.toFixed(1)
+        var y_accel = sensor.y
+        const accely = document.querySelector("#y")
+          accely.innerHTML = "Your y Axis is: " + y_accel.toFixed(1)
+        var z_accel = sensor.z
+        const accelz = document.querySelector("#z")
+          accelz.innerHTML = "Your z Axis is: " + z_accel.toFixed(1)
+    };
   }
 }
 
