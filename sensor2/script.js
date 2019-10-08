0e9c4988a311b5e79e4f7fa169eeebd365ba8eee
kyle1james <kyle1james@gmail.com>
2019-10-08 09:54:20 -0400

    wow

--- sensor2/script.js
+++ sensor2/script.js
@@ -8,6 +8,12 @@ sensor.onreading = () => {
     var x_accel = sensor.x
     const accelX = document.querySelector("#x")
       accelX.innerHTML = "Your X Axis is: " + x_accel.toFixed(1)
+    var y_accel = sensor.y
+    const accely = document.querySelector("#y")
+      accely.innerHTML = "Your y Axis is: " + y_accel.toFixed(1)
+    var z_accel = sensor.z
+    const accelz = document.querySelector("#z")
+      accelz.innerHTML = "Your z Axis is: " + z_accel.toFixed(1)
 };
 
 sensor.onerror = event => console.log(event.error.name, event.error.message);
