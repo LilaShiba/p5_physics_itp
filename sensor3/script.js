37a69b7e02d3ffc11d60c8bddd3a82bf67c199b3
kyle1james <kyle1james@gmail.com>
2019-10-08 15:54:04 -0400

    wow

--- sensor3/script.js
+++ sensor3/script.js
@@ -90,9 +90,7 @@ function deviceMotionHandler(yourMotion){
     }
     
   } else{
-    const accelX = document.querySelector("#x")
-      accelX.innerHTML = " Oh boy, 1.) Please go to settings, 2.)select Safari, 3.)and enable motion and orientation access"
-  }
+  console.log('update safari os')
 }
 
 
